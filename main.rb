4dd9a46ba7ed844c9984f2691438b134389f5f55
Ray Suyoto <ray.suy@icloud.com>
2018-02-09 15:59:37 -0700

    exit menu

--- main.rb
+++ main.rb
@@ -1,10 +1,23 @@
-def welcome 
+
+    
+def welcome_menu
     puts "Welcome to the Magic Eight Ball!"
     puts "To exit at any time type 'exit'"
     puts "Ask me a question!"
-        question = get.strip.downcase
-
+    case
+    when question = get.strip.downcase
+        puts "#{@all.sample}"
+    end
 end
-welcome 
 
+def exit_menu
+    puts "Confirm exit by typing 'exit'"
+    case
+    when gets.strip.downcase
+        puts "Goodbye!"
+        exit 
+    end
+end
 
+exit_menu
+welcome_menu
