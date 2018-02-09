030d468e66b26ad998a234c63d8ed61e86550630
Lindsay Block <lindsay.block05@gmail.com>
2018-02-09 15:23:46 -0700

    making main menu

---
+++ main.rb
@@ -0,0 +1,10 @@
+def welcome 
+    puts "Welcome to the Magic Eight Ball!"
+    puts "To exit at any time type 'exit'"
+    puts "Ask me a question!"
+        question = get.strip.downcase
+
+end
+welcome 
+
+
